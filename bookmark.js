bfef4c955d665f4b63ca97812988966025af17ac
Max Orlov <68651897+ZeusBlockTuber@users.noreply.github.com>
2023-02-18 00:31:07 +0300

    Update bookmark.js

--- bookmark.js
+++ bookmark.js
@@ -15,9 +15,9 @@ clear.addEventListener("click", function () {
 });
 
 function addBookmark() {
-  var input = document.getElementById("searchInput").value;
+  let input = document.getElementById("searchInput").value;
   if (input) {
-    var bookmarkItem = document.createElement("div");
+    let bookmarkItem = document.createElement("div");
     bookmarkItem.className = "bookmark-item";
     bookmarkItem.innerText = input;
     document.querySelector(".bookmark-list").appendChild(bookmarkItem);
@@ -28,16 +28,16 @@ function addBookmark() {
     });
 
     // save bookmarks to localStorage
-    var bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+    let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
     bookmarks.push(input);
     localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
   }
 }
 
 function loadBookmarks() {
-  var bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+  let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
   bookmarks.forEach(function(bookmark) {
-    var bookmarkItem = document.createElement("div");
+    let bookmarkItem = document.createElement("div");
     bookmarkItem.className = "bookmark-item";
     bookmarkItem.innerText = bookmark;
     document.querySelector(".bookmark-list").appendChild(bookmarkItem);
@@ -46,7 +46,7 @@ function loadBookmarks() {
     bookmarkItem.addEventListener("click", function() {
       this.remove();
       // remove bookmark from localStorage
-      var bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
+      let bookmarks = JSON.parse(localStorage.getItem("bookmarks")) || [];
       bookmarks.splice(bookmarks.indexOf(bookmark), 1);
       localStorage.setItem("bookmarks", JSON.stringify(bookmarks));
     });
@@ -55,16 +55,4 @@ function loadBookmarks() {
 
 window.addEventListener("load", function() {
   loadBookmarks();
-});
-
-
-
-
-// add keydown event listener to bookmark item
-bookmarkItem.addEventListener("keydown", function(event) {
-if (event.key === " ") {
-var bookmarkText = this.innerText;
-document.getElementById("searchInput").value = bookmarkText;
-event.preventDefault();
-}
 });
